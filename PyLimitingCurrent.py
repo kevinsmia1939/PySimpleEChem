1c05197c8af58aa16c807c38100cea51b04496a5
Kavin Teenakul <andythe_great@protonmail.com>
2025-05-19 01:46:52 +0200

    better file type handling

--- PyLimitingCurrent.py
+++ PyLimitingCurrent.py
@@ -71,8 +71,15 @@ class MainWindow(QMainWindow):
         # Controls: Open button and slider
         control_layout = QVBoxLayout()
         open_button_layout = QHBoxLayout()
-        self.open_button = QPushButton("Open")
-        self.open_button.clicked.connect(self.open_file)
+        # self.open_button = QPushButton("Open")
+        self.open_button = QComboBox(self)
+        self.open_button.setFixedSize(350, 35)
+        self.open_button.setEditable(False)
+        self.open_button.setInsertPolicy(QComboBox.NoInsert)
+        self.open_button.setEnabled(True)
+        self.open_button.setPlaceholderText("Open Files")
+        self.open_button.addItems(("AutoLab(.xlsx)","Biologic(.txt)","Two column spreadsheet or CSV(.xlsx, .ods, .csv)"))
+        self.open_button.textActivated.connect(self.open_file)
 
         self.lsvchoosecombo = QComboBox(self)
         self.lsvchoosecombo.setFixedSize(300, 35)
@@ -180,52 +187,82 @@ class MainWindow(QMainWindow):
         self.lsv_idx = 0  # chosen lsv data index
         self.file_path_list = []
         self.file_name_list = []
-        self.df_save_data = pd.DataFrame() #Save information on the file and slider settings 
-
+        self.df_save_data = pd.DataFrame()
     def open_file(self):
-        multi_file_path, _ = QFileDialog.getOpenFileNames(self,"Open Data File","","Data Files (*.xlsx *.xls *.ods *.csv *.txt)")
-        if not multi_file_path:
-            return
-        # Read data into DataFrame
-        for file_path in multi_file_path:
-            ext = os.path.splitext(file_path)[1].lower()
-            try:
-                if ext in ['.xlsx', '.xls', '.ods']:
-                    df = pd.read_excel(file_path)
-                    df.columns = ['E', 'I']
-                elif ext in ['.csv', '.txt']:
-                    with open(file_path, 'r', newline='') as f:
-                        sample = f.read(1024)
-                        dialect = csv.Sniffer().sniff(sample)
-                        delimiter = dialect.delimiter
-                    df = pd.read_csv(file_path, delimiter=delimiter)
-                    df.columns = ['E', 'I']
-                else:
-                    raise ValueError(f"Unsupported file type: {ext}")
-
-                df_numeric = df.select_dtypes(include=["number"])
-                is_numeric_df = df.shape[1] == df_numeric.shape[1]
-                if not is_numeric_df:
-                    raise ValueError(file_path, "contain non numerical value")
-                if df.shape[1] != 2:
-                    raise ValueError(file_path, "Data must have 2 columns with first column as E and second column as I")
+        if int(self.open_button.currentIndex()) == 0:
+            print("AutoLab")
+            multi_file_path, _ = QFileDialog.getOpenFileNames(self,"Open AutoLab File")
+            if not multi_file_path:
+                return            
+            for file_path in multi_file_path:
+                df = pd.read_excel(file_path)
+                df = df[["WE(1).Potential (V)", "WE(1).Current (A)"]]
+                df.columns = ["E", "I"]
                 self.file_path_list.append(file_path)
                 self.file_name_list.append(os.path.basename(file_path))
                 self.lsv_null_result = pd.DataFrame({'file name': [os.path.basename(file_path)], 'E/I': [np.nan], '1/I': [np.nan], 'E': [np.nan],'I': [np.nan]})
                 self.lsv_result_display = pd.concat([self.lsv_result_display,self.lsv_null_result],axis=0)
+                #Create df for saving slider info
+                self.df_save_null = pd.DataFrame({'file path': [file_path], 'xviewrange start': [np.nan], 'xviewrange end': [np.nan],'slider1': [np.nan], 'range1': [np.nan],'slider2': [np.nan], 'range2': [np.nan],'slider3': [np.nan], 'range3': [np.nan]})
+                self.df_save_data = pd.concat([self.df_save_data,self.df_save_null],axis=0)        
+                self.lsv_result_display.reset_index(drop=True, inplace=True)
+                self.df_save_data.reset_index(drop=True, inplace=True)
+                self.lsv_result_table.setModel(TableModel(self.lsv_result_display))         
+                self.df_combine_E = pd.concat([self.df_combine_E, df["E"]], axis=1)
+                self.df_combine_I = pd.concat([self.df_combine_I, df["I"]], axis=1)
+        
+        
+        
+        
+        
+        
+        # multi_file_path, _ = QFileDialog.getOpenFileNames(self,"Open Data File","","Data Files (*.xlsx *.xls *.ods *.csv *.txt)")
+        # if not multi_file_path:
+        #     return
+        # # Read data into DataFrame
+        # for file_path in multi_file_path:
+        #     ext = os.path.splitext(file_path)[1].lower()
+        #     try:
+        #         if ext in ['.xlsx', '.xls', '.ods']:
+        #             df = pd.read_excel(file_path)
+        #             df.columns = ['E', 'I']
+        #         elif ext in ['.csv', '.txt']:
+        #             with open(file_path, 'red', newline='') as f:
+        #                 sample = f.read(1024)
+        #                 dialect = csv.Sniffer().sniff(sample)
+        #                 delimiter = dialect.delimiter
+        #             df = pd.read_csv(file_path, delimiter=delimiter)
+        #             df.columns = ['E', 'I']
+        #         else:
+        #             raise ValueError(f"Unsupported file type: {ext}")
+
+        #         df_numeric = df.select_dtypes(include=["number"])
+        #         is_numeric_df = df.shape[1] == df_numeric.shape[1]
+        #         if not is_numeric_df:
+        #             raise ValueError(file_path, "contain non numerical value")
+        #         if df.shape[1] != 2:
+        #             raise ValueError(file_path, "Data must have 2 columns with first column as E and second column as I")
+        #         self.file_path_list.append(file_path)
+        #         self.file_name_list.append(os.path.basename(file_path))
+        #         self.lsv_null_result = pd.DataFrame({'file name': [os.path.basename(file_path)], 'E/I': [np.nan], '1/I': [np.nan], 'E': [np.nan],'I': [np.nan]})
+        #         self.lsv_result_display = pd.concat([self.lsv_result_display,self.lsv_null_result],axis=0)
                 
-            except Exception as e:
-                print(f"Error reading file: {e}")
-                continue
+        #         #Create df for saving slider info
+        #         self.df_save_null = pd.DataFrame({'file path': [file_path], 'xviewrange start': [np.nan], 'xviewrange end': [np.nan],'slider1': [np.nan], 'range1': [np.nan],'slider2': [np.nan], 'range2': [np.nan],'slider3': [np.nan], 'range3': [np.nan]})
+        #         self.df_save_data = pd.concat([self.df_save_data,self.df_save_null],axis=0)
+                
+            # except Exception as e:
+            #     print(f"Error reading file: {e}")
+            #     continue
            
             #update table
-            self.lsv_result_display.reset_index(drop=True, inplace=True)
-
-            self.lsv_result_table.setModel(TableModel(self.lsv_result_display))
+            # self.lsv_result_display.reset_index(drop=True, inplace=True)
+            # self.df_save_data.reset_index(drop=True, inplace=True)
+            # self.lsv_result_table.setModel(TableModel(self.lsv_result_display))
             
-            self.df_combine_E = pd.concat([self.df_combine_E, df["E"]], axis=1)
-            self.df_combine_I = pd.concat([self.df_combine_I, df["I"]], axis=1)
-        print(self.lsv_result_display)
+            # self.df_combine_E = pd.concat([self.df_combine_E, df["E"]], axis=1)
+            # self.df_combine_I = pd.concat([self.df_combine_I, df["I"]], axis=1)
+        # print(self.lsv_result_display)
         self.df_E_max = max(self.df_combine_E.max())
         self.df_E_min = min(self.df_combine_E.min())
         self.df_I_max = max(self.df_combine_I.max())
@@ -253,7 +290,7 @@ class MainWindow(QMainWindow):
         self.lsv_chosen_size = len(self.E)
         self.EI = np.flip(self.E/self.I)
         self.invI = np.flip(1/self.I)
-        print(self.lsv_chosen_idx,self.lsv_chosen_name,self.lsv_chosen_size,self.lsv_chosen_file_path)
+        # print(self.lsv_chosen_idx,self.lsv_chosen_name,self.lsv_chosen_size,self.lsv_chosen_file_path)
         self.config_slider()
 
     def config_slider(self):
@@ -310,53 +347,55 @@ class MainWindow(QMainWindow):
 
     def plot(self):
         self.plot_EV_I.clear()
-        self.lsv = self.plot_EV_I.plot(self.invI,self.EI, pen=pg.mkPen('black', width=1.5))
-        self.slider_marker_fit1 = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='r',symbolSize=8)
-        self.slider_marker_fit2 = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='b',symbolSize=8)
-        self.slider_marker_fit3 = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='g',symbolSize=8)
-        self.slider_marker_fit1_range_start = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='r',symbolSize=13)
-        self.slider_marker_fit2_range_start = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='b',symbolSize=13)
-        self.slider_marker_fit3_range_start = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='g',symbolSize=13)
-        self.slider_marker_fit1_range_end = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='r',symbolSize=13)
-        self.slider_marker_fit2_range_end = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='b',symbolSize=13)
-        self.slider_marker_fit3_range_end = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='g',symbolSize=13)
+        self.lsv                            = self.plot_EV_I.plot(self.invI,self.EI, pen=pg.mkPen('black', width=1.5))
+        self.slider_marker_fit1             = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='red',symbolSize=8)
+        self.slider_marker_fit2             = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='blue',symbolSize=8)
+        self.slider_marker_fit3             = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='darkgreen',symbolSize=8)
+        self.slider_marker_fit1_range_start = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='red',symbolSize=13)
+        self.slider_marker_fit2_range_start = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='blue',symbolSize=13)
+        self.slider_marker_fit3_range_start = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='darkgreen',symbolSize=13)
+        self.slider_marker_fit1_range_end   = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='red',symbolSize=13)
+        self.slider_marker_fit2_range_end   = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='blue',symbolSize=13)
+        self.slider_marker_fit3_range_end   = self.plot_EV_I.plot([0],[0],pen=None,symbol='d',symbolBrush='darkgreen',symbolSize=13)
         
-        self.datafit1 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('r', width=1.5, style=QtCore.Qt.DashLine))
-        self.datafit2 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('b', width=1.5, style=QtCore.Qt.DashLine))
-        self.datafit3 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('b', width=1.5, style=QtCore.Qt.DashLine))
-        self.datafit4 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('g', width=1.5, style=QtCore.Qt.DashLine))
+        self.datafit1 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('red', width=1.5, style=QtCore.Qt.DashLine))
+        self.datafit2 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('blue', width=1.5, style=QtCore.Qt.DashLine))
+        self.datafit3 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('blue', width=1.5, style=QtCore.Qt.DashLine))
+        self.datafit4 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('darkgreen', width=1.5, style=QtCore.Qt.DashLine))
 
-        self.point1 = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='r',symbolSize=8)
-        self.point2 = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='g',symbolSize=8)
-        self.midpoint1 = self.plot_EV_I.plot([0],[0],pen=None,symbol='x',symbolBrush='b',symbolSize=13)
+        self.point1     = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='red',symbolSize=8)
+        self.point2     = self.plot_EV_I.plot([0],[0],pen=None,symbol='o',symbolBrush='darkgreen',symbolSize=8)
+        self.midpoint1  = self.plot_EV_I.plot([0],[0],pen=None,symbol='x',symbolBrush='blue',symbolSize=13)
         self.midpointE1 = self.plot_EV_I.plot([0],[0], pen=pg.mkPen('indianred', width=1.5, style=QtCore.Qt.DashLine))
         
     def update_xviewrange(self):
-        xviewrange_slider_start = self.xviewrange.value()[0]
-        xviewrange_slider_end   = self.xviewrange.value()[1]
-        self.low_xviewrange  = self.invI[xviewrange_slider_start]
-        self.high_xviewrange  = self.invI[xviewrange_slider_end]
-        if self.EI[xviewrange_slider_start] >= np.min(self.EI[xviewrange_slider_start:xviewrange_slider_end]):
-            self.low_yviewrange  = np.min(self.EI[xviewrange_slider_start:xviewrange_slider_end])
+        self.xviewrange_slider_start  = self.xviewrange.value()[0]
+        self.xviewrange_slider_end    = self.xviewrange.value()[1]
+        self.low_xviewrange           = self.invI[self.xviewrange_slider_start]
+        self.high_xviewrange          = self.invI[self.xviewrange_slider_end]
+        
+        if self.EI[self.xviewrange_slider_start] >= np.min(self.EI[self.xviewrange_slider_start:self.xviewrange_slider_end]):
+            self.low_yviewrange  = np.min(self.EI[self.xviewrange_slider_start:self.xviewrange_slider_end])
         else:
             self.low_yviewrange  = self.EI[self.xviewrange.value()[0]]
             
-        if self.EI[xviewrange_slider_end] <= np.max(self.EI[xviewrange_slider_start:xviewrange_slider_end]):
-            self.high_yviewrange  = np.max(self.EI[xviewrange_slider_start:xviewrange_slider_end])
+        if self.EI[self.xviewrange_slider_end] <= np.max(self.EI[self.xviewrange_slider_start:self.xviewrange_slider_end]):
+            self.high_yviewrange  = np.max(self.EI[self.xviewrange_slider_start:self.xviewrange_slider_end])
         else:
             self.high_yviewrange  = self.EI[self.xviewrange.value()[1]]        
             
         self.plot_EV_I.setXRange(self.low_xviewrange,self.high_xviewrange,padding=0)
         self.plot_EV_I.setYRange(self.low_yviewrange,self.high_yviewrange,padding=0.2)
-
+        
+        self.save_data()
 
     def update_marker(self):
-        start1 = self.sliderfit1.value()-self.sliderfit1_range.value()
-        end1 = self.sliderfit1.value()+self.sliderfit1_range.value()
-        start2 = self.sliderfit2.value()-self.sliderfit2_range.value()
-        end2 = self.sliderfit2.value()+self.sliderfit2_range.value()
-        start3 = self.sliderfit3.value()-self.sliderfit3_range.value()
-        end3 = self.sliderfit3.value()+self.sliderfit3_range.value()
+        start1  = self.sliderfit1.value()-self.sliderfit1_range.value()
+        end1    = self.sliderfit1.value()+self.sliderfit1_range.value()
+        start2  = self.sliderfit2.value()-self.sliderfit2_range.value()
+        end2    = self.sliderfit2.value()+self.sliderfit2_range.value()
+        start3  = self.sliderfit3.value()-self.sliderfit3_range.value()
+        end3    = self.sliderfit3.value()+self.sliderfit3_range.value()
         
         self.slider_marker_fit1_range_start.setData([self.invI[start1]],[self.EI[start1]])
         self.slider_marker_fit1_range_end.setData([self.invI[end1]],[self.EI[end1]])
@@ -376,7 +415,6 @@ class MainWindow(QMainWindow):
             poly1 = np.poly1d(coeff1)
             y_fit1 = poly1(self.invI[lnfit1[0]:lnfit1[-1]])
             self.datafit1.setData(self.invI[lnfit1[0]:lnfit1[-1]],y_fit1)
-            # print(coeff1)
         except TypeError:
             pass  
         
@@ -385,7 +423,6 @@ class MainWindow(QMainWindow):
             poly2 = np.poly1d(coeff2)
             y_fit2 = poly2(self.invI[lnfit1[0]:lnfit2[-1]])
             self.datafit2.setData(self.invI[lnfit1[0]:lnfit2[-1]],y_fit2)
-            # print(poly1)
         except TypeError:
             pass
 
@@ -394,7 +431,6 @@ class MainWindow(QMainWindow):
             poly3 = np.poly1d(coeff3)
             y_fit3 = poly3(self.invI[lnfit2[0]:lnfit2[-1]])
             self.datafit4.setData(self.invI[lnfit2[0]:lnfit2[-1]],y_fit3)
-            # print(poly3)
         except TypeError:
             pass 
 
@@ -427,7 +463,20 @@ class MainWindow(QMainWindow):
             self.lsv_result_table.setModel(TableModel(self.lsv_result_display))
             
         except UnboundLocalError:
-            pass      
+            pass    
+        self.save_data()
+        
+        
+    def save_data(self):
+        self.df_save_data.at[self.lsv_chosen_idx, 'xviewrange start']   = self.xviewrange_slider_start
+        self.df_save_data.at[self.lsv_chosen_idx, 'xviewrange end']     = self.xviewrange_slider_end        
+        self.df_save_data.at[self.lsv_chosen_idx, 'slider1']            = self.sliderfit1.value()
+        self.df_save_data.at[self.lsv_chosen_idx, 'slider2']            = self.sliderfit2.value()
+        self.df_save_data.at[self.lsv_chosen_idx, 'slider3']            = self.sliderfit3.value()
+        self.df_save_data.at[self.lsv_chosen_idx, 'range1']             = self.sliderfit1_range.value()
+        self.df_save_data.at[self.lsv_chosen_idx, 'range2']             = self.sliderfit2_range.value()
+        self.df_save_data.at[self.lsv_chosen_idx, 'range3']             = self.sliderfit3_range.value()
+        print(self.df_save_data.to_string())
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
